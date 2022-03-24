ba3a6c594e9f57538cb9bf829815a3101fe69690
maxice8 <thinkabit.ukim@gmail.com>
2022-03-23 21:28:08 -0300

    feat(flake): add nixosModule definitions

--- flake.nix
+++ flake.nix
@@ -9,23 +9,30 @@
     let
       inherit (flake-utils.lib) eachSystem system;
     in
-    eachSystem [ system.x86_64-linux ] (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
-      in
-      {
-        packages = {
-          asusctl = pkgs.callPackage ./pkgs/asusctl { };
-          supergfxctl = pkgs.callPackage ./pkgs/supergfxctl { };
-          gnome-text-editor = pkgs.callPackage ./pkgs/gnome-text-editor { };
-        };
-        overlays = final: prev: {
-          # Inherit the packages into the overlay
-          inherit (self.packages.${system})
-            asusctl supergfxctl gnome-text-editor;
-        };
-      }
-    );
+    eachSystem [ system.x86_64-linux ]
+      (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        in
+        {
+          packages = {
+            asusctl = pkgs.callPackage ./pkgs/asusctl { };
+            supergfxctl = pkgs.callPackage ./pkgs/supergfxctl { };
+            gnome-text-editor = pkgs.callPackage ./pkgs/gnome-text-editor { };
+          };
+          overlays = final: prev: {
+            # Inherit the packages into the overlay
+            inherit (self.packages.${system})
+              asusctl supergfxctl gnome-text-editor;
+          };
+        }
+      ) //
+    {
+      nixosModules = {
+        asusctl = import ./modules/asusctl.nix;
+        supergfxctl = import ./modules/supergfxctl.nix;
+      };
+    };
 }
